2599654e2546be302143e2b94a83a1c828c674f0
许学普 <xuxuepu@scf365.com>
2017-02-15 20:03:02 +0800

    修改

--- server.js
+++ server.js
@@ -1,60 +1,25 @@
-/**
- * Created by 胖墩 on 2016/8/30.
- */
-var $ = require('jquery');
-var _ = require('underscore');
-var vm = require('vm');
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
 var fs = require('fs');
-var journey = require('journey');
-var async = require('async');
-var dbutil = require('./dbutil');
 
-String.prototype.replaceAll = function(s1, s2) {
-    var demo = this
-    while (demo.indexOf(s1) != - 1)
-        demo = demo.replace(s1, s2);
-    return demo;
-}
-// Create a Router
-var router = new(journey.Router);
+app.listen(1989);
 
-// Create the routing table
-router.map(function() {
-    // this.root.bind(function (req, res) { res.send("Welcome") });
-    this.get(/HamstrerServlet\/(\w*\W*\w*)*/).bind(function(req, res, id) {
-        var runJsPath = this.request.url.pathname.replaceAll("/HamstrerServlet", "") + ".js";
-        console.log("执行的脚本文件:" + runJsPath);
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
 
-        //传入的绑定变量
-        var sandbox = {
-            req: req,
-            res: res,
-            $: $,
-            dbutil: dbutil,
-            async: async,
-            console: console
-        };
-
-        fs.readFile('./HamstrerServlet' + runJsPath, function(err, data) {
-            vm.runInNewContext(data, sandbox, 'myfile.vm');
+            res.writeHead(200);
+            res.end(data);
         });
+}
 
-    });
-    this.post('/^HamstrerServlet\/(\w*)$/)').bind(function(req, res, data) {
-        res.send(200);
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
     });
 });
-
-require('http').createServer(function(request, response) {
-    var body = "";
-
-    request.addListener('data', function(chunk) {
-        body += chunk
-    });
-    request.addListener('end', function() {
-        router.handle(request, body, function(result) {
-            response.writeHead(result.status, result.headers);
-            response.end(result.body);
-        });
-    });
-}).listen(8080);
