d15eb11ff4b803f51b24077e2211b8d6b75d5b91
xuxuepu <xuxuepu@scf365.com>
2017-09-30 15:02:05 +0800

    测试接口

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,6 +1,13 @@
 module.exports = function( app ){
     app.get('/', function(req, res){
-        var json = {data:'index'};
+        var json = {
+            code: 0,
+            data: {
+                msg: "专业写500几十年，经历过500的接口才是最健壮的接口。",
+                author: "epo",
+                create_date: "2017-09-30"
+            }
+        };
         res.send(json);
     });
 };
